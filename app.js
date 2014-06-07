847f2c45bec6d8c178661124e8dd4a7a02a01d15
James Shepard <jrshepard@gmail.com>
2014-06-06 21:34:24 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+(function(){
+	var app = angular.module('schooldistrictnotes',[]);
+
+	app.controller('NotesController',function(){
+		this.engagement = school;
+	});
+
+	var school = {
+		name: 'Peach County Board of Education',
+		contact: 'Peach Lady',
+		note1: false,
+	}
+
+})();
+
+
+
